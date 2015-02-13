2415311553a3118077cc0390379cfc5f93774d6e
Petrica Clement Chiriac <petrica_chiriac@fluxinternet.ro>
2015-02-13 10:06:32 +0200

    Slides week 2 - Skel

---
+++ week/slide_config.js
@@ -0,0 +1,37 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Android fundamentals',
+    subtitle: 'Study Jam Android',
+    eventTitle: 'GDG Bucuresti Week #2 - 14 February 2015',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Petrica Clement Chiriac',
+    company: 'Manager GDG Bucharest, CEO Fluxinternet SRL',
+    gplus: 'https://plus.google.com/+PetricaClementChiriac',
+    twitter: '@Tica2',
+    www: '',
+    github: 'https://github.com/Tica2'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
